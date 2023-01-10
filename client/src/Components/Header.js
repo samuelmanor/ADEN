bd8c22d33ff3f893e960b680747ab79bbfda73b1
sam <manor@hey.com>
2023-01-10 15:18:26 -0500

    toggle loginui visibility logic

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginUI from "./LoginUI";
 
 function Header() {
+    const [showLoginUI, toggleShowLoginUI] = useState(false);
+
+    function displayLoginUI() {
+        toggleShowLoginUI((showLoginUI) => (!showLoginUI));
+    }
     return (
         <div id='header'>
             <div className='headtab'>about</div>
             <div className='headtab' id='title'>ADEN</div>
-            <div className='headtab'>log in</div>
+            <div className='headtab' onClick={displayLoginUI}>log in</div>
+
+            {showLoginUI ? <LoginUI /> : null}
         </div>
     )
 }
--- client/src/Components/LoginUI.js
+++ client/src/Components/LoginUI.js
@@ -3,7 +3,7 @@ import React from "react";
 function LoginUI() {
     return(
         <div id='loginui'>
-
+            <p>(loginui) enter info here</p>
         </div>
     )
 }
