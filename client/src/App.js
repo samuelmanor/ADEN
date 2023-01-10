3017c65a16bbd30fd7d9ee6788bf897a9f3fb3bf
sam <manor@hey.com>
2023-01-10 15:09:18 -0500

    base header html

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.scss';
+import Header from './Components/Header';
 
+// npm start --prefix client
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
     return (
         <div id='header'>
-
+            <div className='headtab'>about</div>
+            <div className='headtab' id='title'>ADEN</div>
+            <div className='headtab'>log in</div>
         </div>
     )
 }
