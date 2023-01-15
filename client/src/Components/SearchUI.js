e0b3907a942066024f730e15c7b53719002e4c02
sam <manor@hey.com>
2023-01-14 21:23:15 -0500

    successful fetch

--- client/src/Components/SearchUI.js
+++ client/src/Components/SearchUI.js
@@ -6,7 +6,7 @@ function SearchUI() {
     const [listings, setListings] = useState([]);
 
     function getListings() {
-        fetch(`http://localhost:3000/listings/`)
+        fetch(`http://localhost:3000/listings?identity_id=3&service_id=${searchQuery.service}&location_id=${searchQuery.location}`)
         .then((r) => r.json())
         .then((listingsArray) => {
             console.log(listingsArray);
