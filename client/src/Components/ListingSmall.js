82ee3b68d251263a6dea6e3fc63323fa0171202e
sam <manor@hey.com>
2023-01-19 21:22:08 -0500

    listingsmall expanded view logic, css

--- client/src/Components/ListingSmall.js
+++ client/src/Components/ListingSmall.js
@@ -1,14 +1,41 @@
-import React from "react";
-import ListingLarge from "./ListingLarge";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"; // to open in new tab?
+import React, { useState } from "react";
 
-function ListingSmall({ id, name, address, description, website, phone, comments }) {
+function ListingSmall({ name, address, description, website, phone, comments }) {
+    const [shown, toggleShown] = useState(false);
+
+    const large = <div id='listinglarge' className='hiddenlisting'> 
+        <h1 id='x' onClick={toggleExpandView}>x</h1>
+        <h2>{name}</h2>
+        <h3>{address}</h3>
+        <p>{description}</p>
+        <div id='listinginfo'>
+            <h3><a href={'http://' + website}>website</a></h3>
+            <h3>{phone}</h3>
+        </div>
+    </div>
+
+    function toggleExpandView() {
+        toggleShown((shown) => (!shown));
+
+        const box = document.getElementById('listinglarge');
+
+        if (shown) {
+            box.className = '';
+            document.body.style.overflow = 'hidden';
+        } else {
+            box.className = 'hiddenlisting';
+            document.body.style.overflow = '';
+        }
+    }
+
     return (
         <div id='listingsmall'>
-            {/* <h2>{name}</h2>
+            <h2 onClick={toggleExpandView}>{name}</h2>
             <h3>{address}</h3>
             <p>{description}</p>
-            <p className='commentlabel'>{comments.length} comment{comments.length > 1 || comments.length === 0 ? 's' : null}</p> */}
+            <p className='commentlabel'>{comments.length} comment{comments.length > 1 || comments.length === 0 ? 's' : null}</p>
+
+            {large}
         </div>
     )
 }
