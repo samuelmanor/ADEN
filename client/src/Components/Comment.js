29ca288570ee18962c8e6a39208d240fd4f6e323
sam <manor@hey.com>
2023-02-02 23:59:08 -0500

    test

--- client/src/Components/Comment.js
+++ client/src/Components/Comment.js
@@ -3,14 +3,14 @@ import React from "react";
 function Comment({ id, user_id, listing_id, commenttext, author, currentUser }) {
 
     function delComment() {
-        console.log('clicked del on', listing_id, id);
+        console.log('clicked del on', listing_id);
     }
 
     return (
         <div className='comment'>
             <h4>{author}</h4>
             <p>{commenttext}</p>
-            
+
             {author == currentUser.username ? <button onClick={delComment}>x</button> : null}
         </div>
     )
